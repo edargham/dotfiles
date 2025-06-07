3cdcd86943aa6a304cb9fbb2c429ba7e4d8f9c35
Elias Dargham <edargham@hotmail.com>
2025-06-07 13:31:54 +0300

    Refactor Waybar power menu and button styles for improved aesthetics and consistency

--- waybar/power-menu.sh
+++ waybar/power-menu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CHOICE=$(printf "  Shutdown  \n  Restart  \n  Logout  \n  Suspend  \n  Hibernate  " | bemenu -p "  Power Menu  " --fn "JetBrainsMono Nerd Font 12" -H 33 --hp 8 --nb "#000000ab" --nf "#00ffff" --ab "#000000ab" --af "#00ffff" --tb "#ff0040" --tf "#000000" --fb "#000000ab" --ff "#00ffff" --hb "#ffff00ab" --hf "#000000" --sb "#ffff00ab" --sf "#000000" --scb "#000000ab" --scf "#ffff00")
+CHOICE=$(printf "  Shutdown  \n  Restart  \n  Logout  \n  Suspend  \n  Hibernate  " | bemenu -p "  Power Menu  " --fn "JetBrainsMono Nerd Font 12" -H 31 --hp 8 --nb "#000000ab" --nf "#00ffff" --ab "#000000ab" --af "#00ffff" --tb "#ff0040" --tf "#000000" --fb "#000000ab" --ff "#00ffff" --hb "#ffff00ab" --hf "#000000" --sb "#ffff00ab" --sf "#000000" --scb "#000000ab" --scf "#ffff00")
 
 case "$CHOICE" in
     *"Shutdown"*) poweroff ;;
